b46e020ea36e03bd144ca3df926f13fe859a3489
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-10 15:52:41 +0530

    Update Beverages.java

--- Day7/Session1/Beverages.java
+++ Day7/Session1/Beverages.java
@@ -1,28 +1,23 @@
  
-public class simple{
-
- public static void main(String args[]){ 
-   Beverages obj=new Tea();
-obj.addIngredient();
-
-}
+public class simple  {
+      public static void main(String args[]){ 
+        Beverages obj=new Tea();
+       obj.addIngredient();
+      }
 }
 abstract class Beverages{
- public void addMilk()
-{
-System.out.println("Add Milk");
-} 
-abstract public void addIngredient();
-
+        public void addMilk(){
+        System.out.println("Add Milk");
+       } 
+   abstract public void addIngredient();
 }
 class Tea extends Beverages{
-public void addIngredient(){
-    System.out.println("Tea Class");
-}
+              public void addIngredient(){
+              System.out.println("Tea Class");
+                     }
 }
 class Coffee extends Beverages{
-public void addIngredient(){
-    System.out.println("Coffee Class");
-}
-
+     public void addIngredient(){
+      System.out.println("Coffee Class");
+       }
 }
