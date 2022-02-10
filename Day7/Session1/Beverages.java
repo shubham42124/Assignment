4152edcde8e387150ca8525dc84e5b780ffdc674
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-10 10:25:40 +0530

    Add files via upload

---
+++ Day7/Session1/Beverages.java
@@ -0,0 +1,28 @@
+ 
+public class simple{
+
+ public static void main(String args[]){ 
+   Beverages obj=new Tea();
+obj.addIngredient();
+
+}
+}
+abstract class Beverages{
+ public void addMilk()
+{
+System.out.println("Add Milk");
+} 
+abstract public void addIngredient();
+
+}
+class Tea extends Beverages{
+public void addIngredient(){
+    System.out.println("Tea Class");
+}
+}
+class Coffee extends Beverages{
+public void addIngredient(){
+    System.out.println("Coffee Class");
+}
+
+}
