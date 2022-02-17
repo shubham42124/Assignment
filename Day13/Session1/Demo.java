c52cca6d0f96d3218a86f3efc4562a42db552819
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-17 23:52:18 +0530

    Create Demo.java

---
+++ Day13/Session1/Demo.java
@@ -0,0 +1,10 @@
+import java.io.File;
+     import java.io.IOException;
+
+     public class Demo {
+	    public static void main(String[] args) throws IOException {
+		File fo = new File("welcome.text");
+		f0.createNewFile();
+		System.out.println("File is created");
+	     }
+      }
