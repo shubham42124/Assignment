76bc496f67a1a488d77ebd885225fc0200b07bb0
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-17 22:38:01 +0530

    Create HashMapProg.java

---
+++ Day12/Session2/HashMapProg.java
@@ -0,0 +1,21 @@
+import java.util.*;
+  
+public class Hash_Map_Demo {
+    public static void main(String[] args)
+    {
+  
+       
+        HashMap<Integer, String> hash_map = new HashMap<Integer, String>();
+  
+      
+        hash_map.put(10, "manish");
+        hash_map.put(15, "vinay");
+        hash_map.put(20, "ajay");
+        hash_map.put(25, "kanha");
+        hash_map.put(30, "suresh");
+  
+        
+        System.out.println("Initial Mappings are: " + hash_map);
+  
+    }
+}
