5027e50ba31e8a207c73d3ccfc4d81b20d52f488
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-18 00:16:14 +0530

    Create Test.java

---
+++ Day14/Session1/Test.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Test {
+public static void main(String[] args) {
+	
+	Scanner sc= new Scanner(System.in);
+	
+	System.out.println("Enter first number");
+	
+	int fnum=sc.nextInt();
+     System.out.println("Enter second number");
+	
+	int snum=sc.nextInt();
+	
+	
+	int z= fnum/snum;
+	
+	System.out.println("divison of two numbers:"+z);
+	
+
+	
+	
+}
+}
