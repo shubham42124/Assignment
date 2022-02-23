7b30fd6ac6aac5c72eff53c2bdc7684a5b24e758
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-23 17:40:56 +0530

    Create ArrayList1.java

---
+++ Day16/Session1/ArrayList1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayList1 {
+public static void main(String[] args) {
+	
+	ArrayList<String> ar=new ArrayList<String>();
+	
+	ar.add("aman");
+	ar.add("shubham");
+	ar.add("ravi");
+	ar.add("manish");
+	ar.add("akshit");
+	
+	   ar.forEach(  
+	            (n)->System.out.println()  
+	        );  
+}
+}
