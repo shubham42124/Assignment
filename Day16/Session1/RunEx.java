e1ad00d83d9aee1f2b3abd7baa72d5f9a7b97573
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-23 17:30:58 +0530

    Create RunEx.java

---
+++ Day16/Session1/RunEx.java
@@ -0,0 +1,22 @@
+
+
+
+
+public class RunEx {
+public static void main(String[] args) {
+	
+	Runnable r=()->{
+		
+		for (int i = 0; i <10; i++) {
+			System.out.println("child thread");
+		}
+	};
+	
+	Thread t= new Thread(r);
+	t.start();
+	
+	for (int i = 0; i <10; i++) {
+		System.out.println("main thread");
+	}
+}
+}
